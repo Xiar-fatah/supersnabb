ce53e84d84f15db6436452b27037f28633d65eaf
Xiar-fatah <xiar.fatah@gmail.com>
2024-02-25 13:08:39 +0100

    FixedLeg completed

--- supersnabb/time/daycounters/act360.py
+++ supersnabb/time/daycounters/act360.py
@@ -7,7 +7,6 @@ class ACT360(Daycounter):
         super().__init__()
 
     def year_fraction(self, start_date: Date, end_date: Date) -> float:
-        print(f"{(end_date.serial_number - start_date.serial_number) = }")
         return (end_date.serial_number - start_date.serial_number) / 360
 
     def day_count(self, start_date: Date, end_date: Date) -> int:
